4f11a5c6d9cdd7a2fcf759da6b68fae2aba275f9
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2023-12-05 15:04:15 +0900

    init project

---
+++ src/main/java/com/study/snsbackoffice/SnsbackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.study.snsbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsbackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsbackofficeApplication.class, args);
+	}
+
+}
