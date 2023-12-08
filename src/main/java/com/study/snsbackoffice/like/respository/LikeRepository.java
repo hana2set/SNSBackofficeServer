b47d0e9ebafaf31d3c75515e578ff637b836705e
minseong1 <alstjd2627@naver.com>
2023-12-08 19:21:50 +0900

    init

--- src/main/java/com/study/snsbackoffice/like/respository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.snsbackoffice.like.respository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Long, Like> {
-}
